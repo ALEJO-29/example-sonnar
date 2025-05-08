8e645fc0a435472ba86a4ea4d259747b945ee15d
ALEJO29 <alejandro123aut@gmail.com>
2025-05-08 14:51:06 -0500

    feat: update sonarcloud workflow and dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
-    include: ['src/**/__test__/**/*.spec.{js,jsx}'],
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
+    include: ["src/**/__test__/**/*.{js,jsx}"],
+    exclude: ["**/setupTests.js"],
+    coverage: {
+      include: ["src/**/*.{js}"],
+      exclude: ["src/setupTests.js", "src/App.jsx", "src/main.jsx"],
+    },
   },
-})
+});
